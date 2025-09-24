867215132a59c8b7bbd6bd83671c82b1740f3814
rawinolivera <oli18001@byui.edu>
2025-09-24 00:26:06 -0300

    embers data

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,4 @@
-const requestURL = "https://rawinolivera.github.io/wdd231/chamber/scripts/members.json";
+const requestURL = "https://rawinolivera.github.io/wdd231/chamber/data/members.json";
 const dir = document.querySelector("#directory");
 
 fetch(requestURL)
@@ -11,5 +11,30 @@ fetch(requestURL)
   });
 
   function displayMembers(member) {
-    
+    let card = document.createElement("section");
+    let name = document.createElement("p");
+    let address = document.createElement("p");
+    let phoneNumber = document.createElement("p");
+    let url = document.createElement("a");
+    let image = document.createElement("img");
+    let membership = document.createElement("p");
+    let otherInfo = document.createElement("p");
+
+    name.textContent = `${member.name}`;
+    address.textContent = `${member.address}`;
+    phoneNumber.textContent = `${member.phone_number}`;
+    url.textContent = `${member.website_url}`;
+    image.src = `${member.icon_url}`;
+    image.alt = `Company Offial Logo`;
+    membership.textContent = `${member.membership_level}`;
+    otherInfo.textContent = `${member.other_info}`;
+
+    card.appendChild(name); 
+    card.appendChild(address); 
+    card.appendChild(phoneNumber); 
+    card.appendChild(url); 
+    card.appendChild(image); 
+    card.appendChild(membership); 
+    card.appendChild(otherInfo); 
+    dir.appendChild(card); 
   }
