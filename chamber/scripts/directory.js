2cfccffcb9a6ac2cfc61822561ede5cb3b9e0445
rawinolivera <oli18001@byui.edu>
2025-09-23 23:59:06 -0300

    embers data

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,1 +1,15 @@
 const requestURL = "https://rawinolivera.github.io/wdd231/chamber/scripts/members.json";
+const dir = document.querySelector("#directory");
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const members = jsonObject['members'];
+    members.forEach(displayMembers);
+  });
+
+  function displayMembers(member) {
+    
+  }
