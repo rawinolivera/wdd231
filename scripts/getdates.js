6fcf78ab413360c716d6a00ae62840c12e5cd5b8
rawinolivera <oli18001@byui.edu>
2025-10-12 00:12:45 -0300

    Individual Project Site Plan

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+var date = new Date();
+var year = date.getFullYear();
+document.querySelector('#current-year').textContent = year;
+document.querySelector('#last-mod').textContent = document.lastModified;
