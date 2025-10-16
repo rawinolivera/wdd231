cfb4247067557a00aa9696ed92a1812d1e65f119
rawinolivera <oli18001@byui.edu>
2025-10-16 07:50:40 -0300

    Final Project

--- final/scripts/base.js
+++ final/scripts/base.js
@@ -23,6 +23,94 @@ window.addEventListener("resize", () => {
 });
 
 /*   Menu location   */
+function homeItems() {
+
+  //Get lat and lon
+  const lat = -23.55052;
+  const lon = -46.633308;
+
+  /*------  WEATHER API  ------*/
+  const apiKey = '0c8a48091627e17ece4ad85f202a2799'
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let te = jsObject.main.temp;
+    let sp = jsObject.wind.speed;
+    document.querySelector('#temp').textContent = `${te} °C`;  //t
+    document.querySelector('#wind').textContent = `${sp} Km`;  //s
+
+
+    const desc = jsObject.weather[0].description; //w-now
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
+    const icon = document.querySelector(".icon");
+    let img = document.createElement("img");
+    img.setAttribute('src', iconsrc);
+    img.setAttribute('alt', desc);
+
+    icon.appendChild(img);
+    document.querySelector('#condit').textContent = desc;
+
+    let windchill = "";
+
+    if (te < 10 && sp >= 4.8){
+        windchill = windChill(te, sp);
+        windchill = `${windchill} °C`;
+    } else {
+        windchill = "N/A";
+    } 
+    //output
+    document.querySelector("#w-chill").innerHTML = windchill;
+
+    function windChill(te,sp) {
+      return Math.round((13.12 + 0.6215 * te - 11.37 * Math.pow(sp, 0.16) + 0.3965 * te * Math.pow(sp, 0.16)) * 100) / 100;
+    }
+  }); 
+
+
+  //Random Quotes Data
+  const quotes = [
+    {
+      "quote": "Programs are primarily meant to be read and understood by humans first, and only incidentally executed by computers for results.",
+      "author": "Donald Knuth"
+    },
+    {
+      "quote": "There are only two kinds of programming languages: those that people always complain about endlessly and those that nobody ever actually uses.",
+      "author": "Bjarne Stroustrup"
+    },
+    {
+      "quote": "The most dangerous phrase in the programming language is always, 'We have always done it this way,' which stops innovation and learning.",
+      "author": "Grace Hopper"
+    },
+    {
+      "quote": "Talk is cheap and anyone can say many things, but what truly matters in software development is to actually show me the working code.",
+      "author": "Linus Torvalds"
+    },
+    {
+      "quote": "Writing good code is not only about functionality but also about readability and maintainability, because good code is its own best documentation.",
+      "author": "Steve McConnell"
+    },
+    {
+      "quote": "First, fully understand and solve the problem at hand with clear thinking, and only afterwards proceed to write the actual code implementation efficiently.",
+      "author": "John Johnson"
+    }
+  ]
+
+  // Random quote 
+  function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  }
+  const randomQuote = getRandomQuote();
+
+  document.querySelector(".quote").textContent = `${randomQuote.quote}`;
+  document.querySelector(".autor").textContent = `— ${randomQuote.author}`;
+
+}
+
+
 const links = document.querySelectorAll("nav a");
 const currentPage = window.location.pathname.split("/").pop();
 
@@ -34,85 +122,6 @@ links.forEach(link => {
   }
 });
 
-//Get lat and lon
-const lat = -23.55052;
-const lon = -46.633308;
-
-/*------  WEATHER API  ------*/
-const apiKey = '0c8a48091627e17ece4ad85f202a2799'
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
-fetch(apiURL)
-.then((response) => response.json())
-.then((jsObject) => {
-  let te = jsObject.main.temp;
-  let sp = jsObject.wind.speed;
-  document.querySelector('#temp').textContent = `${te} °C`;  //t
-  document.querySelector('#wind').textContent = `${sp} Km`;  //s
-
-
-  const desc = jsObject.weather[0].description; //w-now
-  const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-
-  const icon = document.querySelector(".icon");
-  let img = document.createElement("img");
-  img.setAttribute('src', iconsrc);
-  img.setAttribute('alt', desc);
-
-  icon.appendChild(img);
-  document.querySelector('#condit').textContent = desc;
-
-  let windchill = "";
-
-  if (te < 10 && sp >= 4.8){
-      windchill = windChill(te, sp);
-      windchill = `${windchill} °C`;
-  } else {
-      windchill = "N/A";
-  } 
-  //output
-  document.querySelector("#w-chill").innerHTML = windchill;
-
-  function windChill(te,sp) {
-    return Math.round((13.12 + 0.6215 * te - 11.37 * Math.pow(sp, 0.16) + 0.3965 * te * Math.pow(sp, 0.16)) * 100) / 100;
-  }
-}); 
-
-
-//Random Quotes Data
-const quotes = [
-  {
-    "quote": "Programs are primarily meant to be read and understood by humans first, and only incidentally executed by computers for results.",
-    "author": "Donald Knuth"
-  },
-  {
-    "quote": "There are only two kinds of programming languages: those that people always complain about endlessly and those that nobody ever actually uses.",
-    "author": "Bjarne Stroustrup"
-  },
-  {
-    "quote": "The most dangerous phrase in the programming language is always, 'We have always done it this way,' which stops innovation and learning.",
-    "author": "Grace Hopper"
-  },
-  {
-    "quote": "Talk is cheap and anyone can say many things, but what truly matters in software development is to actually show me the working code.",
-    "author": "Linus Torvalds"
-  },
-  {
-    "quote": "Writing good code is not only about functionality but also about readability and maintainability, because good code is its own best documentation.",
-    "author": "Steve McConnell"
-  },
-  {
-    "quote": "First, fully understand and solve the problem at hand with clear thinking, and only afterwards proceed to write the actual code implementation efficiently.",
-    "author": "John Johnson"
-  }
-]
-
-// Random quote 
-function getRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  return quotes[randomIndex];
+if (currentPage === 'index.html') {
+  homeItems();
 }
-const randomQuote = getRandomQuote();
-
-document.querySelector(".quote").textContent = `${randomQuote.quote}`;
-document.querySelector(".autor").textContent = `— ${randomQuote.author}`;
--- final/scripts/languages.js
+++ final/scripts/languages.js
@@ -1,6 +1,7 @@
 function displayLanguages(lang, container) {
 let card = document.createElement("section");
-let dialog = document.querySelector("dialog");
+let dialog = document.createElement("dialog");
+let title = document.createElement("h2");
 const di = document.querySelector("#di");
 //let figure = document.createElement("figure");
 //let image = document.createElement("img");
@@ -8,22 +9,26 @@ let name = document.createElement("h2");
 let paradig = document.createElement("p");
 let org = document.createElement("p");
 let year = document.createElement("h3");
-let history = document.querySelector("p")
+let history = document.createElement("p")
 let btnOpen = document.createElement("button");
 let btnClose = document.createElement("button");
 
 
 //image.className = "img";
 //image.loading ="lazy";
-//image.src = `${place.imagelink}`;
-//image.alt = `${place.name} photo`;
-dialog.id = `${place.id}`;
-name.textContent = `${place.name}`;
+//image.src = `${lang.imagelink}`;
+//image.alt = `${lang.name} photo`;
+dialog.id = `${lang.id}`;
+title.textContent = `${lang.name}`;
+btnClose.id = `close-${lang.id}`;
+btnClose.textContent = "Close";
+name.textContent = `${lang.name} Language`;
 name.className = "m-name";
-paradig.textContent = `${place.paradigm}`;
-org.textContent = `${place.organization}`;
-year.textContent = `${place.year}`;
-history.textContent = `${place.history}`;
+paradig.textContent = `${lang.paradigm}`;
+org.textContent = `${lang.organization}`;
+year.textContent = `Release year: ${lang.year}`;
+history.textContent = `${lang.history}`;
+btnOpen.id = `open-${lang.id}`;
 btnOpen.textContent = "Learn More";
 
 //figure.appendChild(image);
@@ -31,11 +36,11 @@ btnOpen.textContent = "Learn More";
 card.appendChild(name);
 card.appendChild(org);
 card.appendChild(year);
-card.appendChild(button);
+card.appendChild(btnOpen);
 
 container.appendChild(card);
 
-dialog.appendChild(name);
+dialog.appendChild(title);
 dialog.appendChild(history);
 dialog.appendChild(btnClose)
 
@@ -44,15 +49,15 @@ di.appendChild(dialog);
 }
 
 //data
-const requestURL = "https://rawinolivera.github.io/wdd231/chamber/data/languages.json";
-const dir = document.querySelector("#interest");
+const requestURL = "https://rawinolivera.github.io/wdd231/final/data/lenguages.json";
+const dir = document.querySelector("#lang");
 
 async function loadInterest() {
   try {
     const response = await fetch(requestURL);
     const jsonObject = await response.json();
-    const places = jsonObject['places'];
-    places.forEach(p => displayPlaces(p, dir));
+    const languages = jsonObject['languages'];
+    languages.forEach(l => displayLanguages(l, dir));
   } catch (error) {
     console.error("Interest Places not found:", error);
   }
