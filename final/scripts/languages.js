6e33facd1f095e4a34937d161872029739121960
rawinolivera <oli18001@byui.edu>
2025-10-16 10:38:17 -0300

    Final Project

--- final/scripts/languages.js
+++ final/scripts/languages.js
@@ -1,56 +1,54 @@
 function displayLanguages(lang, container) {
-let card = document.createElement("section");
-let dialog = document.createElement("dialog");
-let title = document.createElement("h2");
-const di = document.querySelector("#di");
-//let figure = document.createElement("figure");
-//let image = document.createElement("img");
-let name = document.createElement("h2");
-let paradig = document.createElement("p");
-let org = document.createElement("p");
-let year = document.createElement("h3");
-let history = document.createElement("p")
-let btnOpen = document.createElement("button");
-let btnClose = document.createElement("button");
+  let card = document.createElement("section");
+  let dialog = document.createElement("dialog");
+  let title = document.createElement("h2");
+  const di = document.querySelector("#di");
+  let name = document.createElement("h2");
+  let paradig = document.createElement("p");
+  let org = document.createElement("p");
+  let year = document.createElement("h3");
+  let history = document.createElement("p")
+  let btnOpen = document.createElement("button");
+  let btnClose = document.createElement("button");
 
 
-dialog.id = `${lang.id}`;
-title.textContent = `${lang.name} Language`;
-btnClose.id = `close-${lang.id}`;
-btnClose.textContent = "Close";
-name.textContent = `${lang.name} Language`;
-name.className = "m-name";
-paradig.textContent = `Paradig: ${lang.paradigm}`;
-org.textContent = `${lang.organization}`;
-year.textContent = `Release year: ${lang.year}`;
-history.textContent = `${lang.history}`;
-btnOpen.id = `open-${lang.id}`;
-btnOpen.textContent = "Learn More";
+  dialog.id = `${lang.id}`;
+  title.textContent = `${lang.name} Language`;
+  btnClose.id = `close-${lang.id}`;
+  btnClose.textContent = "Close";
+  name.textContent = `${lang.name} Language`;
+  name.className = "m-name";
+  paradig.textContent = `Paradig: ${lang.paradigm}`;
+  org.textContent = `${lang.organization}`;
+  year.textContent = `Release year: ${lang.year}`;
+  history.textContent = `${lang.history}`;
+  btnOpen.id = `open-${lang.id}`;
+  btnOpen.textContent = "Learn More";
 
 
-card.appendChild(name);
-card.appendChild(org);
-card.appendChild(year);
-card.appendChild(btnOpen);
+  card.appendChild(name);
+  card.appendChild(org);
+  card.appendChild(year);
+  card.appendChild(btnOpen);
 
-container.appendChild(card);
+  container.appendChild(card);
 
-dialog.appendChild(title);
-dialog.appendChild(paradig)
-dialog.appendChild(history);
-dialog.appendChild(btnClose)
+  dialog.appendChild(title);
+  dialog.appendChild(paradig)
+  dialog.appendChild(history);
+  dialog.appendChild(btnClose)
 
-di.appendChild(dialog);
+  di.appendChild(dialog);
 
-btnOpen.addEventListener("click", () => {
-  const dialog = document.getElementById(lang.id);
-  if (dialog) dialog.showModal();
-});
+  btnOpen.addEventListener("click", () => {
+    const dialog = document.getElementById(lang.id);
+    if (dialog) dialog.showModal();
+  });
 
-btnClose.addEventListener("click", () => {
-  const dialog = document.getElementById(lang.id);
-  if (dialog) dialog.close();
-});
+  btnClose.addEventListener("click", () => {
+    const dialog = document.getElementById(lang.id);
+    if (dialog) dialog.close();
+  });
 
 }
 
